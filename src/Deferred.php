2761179ec8fd286f969f7d125f02ee267a2ff8f0
Andri Thorlacius <andri.thorlacius@gmail.com>
2026-04-13 21:50:11 +0000

    feat: Add parallel async Deferred execution support
    Adds Deferred::createAsync() which accepts an async callback returning
    Awaitable<mixed>. When runQueue() encounters any async deferreds, it
    fires all async callbacks in parallel via HH\Asio\v() while running
    sync deferreds sequentially. This enables parallel DB query execution
    for GraphQL field resolution without changing the sync API.

--- src/Deferred.php
+++ src/Deferred.php
@@ -5,19 +5,10 @@ use GraphQL\Executor\Promise\Adapter\SyncPromise;
 
 class Deferred
 {
-    /**
-     * @var \SplQueue
-     */
     private static ?\SplQueue $queue;
 
-    /**
-     * @var callable
-     */
     private (function():mixed) $callback;
 
-    /**
-     * @var SyncPromise
-     */
     public SyncPromise $promise;
 
     public static function getQueue()
@@ -28,13 +19,87 @@ class Deferred
     public static function runQueue()
     {
         $q = self::$queue;
-        while ($q && !$q->isEmpty())
+        if ($q === null || $q->isEmpty())
         {
-            /** @var self $dfd */
-            $dfd = $q->dequeue();
-            $dfd->run();
+            return;
+        }
+
+        $deferreds = [];
+        while (!$q->isEmpty())
+        {
+            $deferreds[] = $q->dequeue();
+        }
+
+        $hasAsync = false;
+        foreach ($deferreds as $dfd)
+        {
+            if ($dfd->async)
+            {
+                $hasAsync = true;
+                break;
+            }
+        }
+
+        if ($hasAsync)
+        {
+            self::runQueueAsync($deferreds);
+        }
+        else
+        {
+            foreach ($deferreds as $dfd)
+            {
+                $dfd->run();
+            }
         }
     }
+
+    private static function runQueueAsync(array<Deferred> $deferreds):void
+    {
+        $awaitables = [];
+        $syncDeferreds = [];
+
+        foreach ($deferreds as $dfd)
+        {
+            if ($dfd->async)
+            {
+                $awaitables[] = \Pair {$dfd, $dfd->runAsync()};
+            }
+            else
+            {
+                $syncDeferreds[] = $dfd;
+            }
+        }
+
+        foreach ($syncDeferreds as $dfd)
+        {
+            $dfd->run();
+        }
+
+        if (\count($awaitables) > 0)
+        {
+            $asyncAwaitables = [];
+            foreach ($awaitables as $pair)
+            {
+                $asyncAwaitables[] = $pair[1];
+            }
+            $results = \HH\Asio\join(\HH\Asio\v($asyncAwaitables));
+            foreach ($awaitables as $i => $pair)
+            {
+                $dfd = $pair[0];
+                try
+                {
+                    $dfd->promise->resolve($results[$i]);
+                }
+                catch (\Exception $e)
+                {
+                    $dfd->promise->reject($e);
+                }
+            }
+        }
+    }
+
+    private bool $async = false;
+    private ?(function():Awaitable<mixed>) $asyncCallback = null;
 
     public function __construct((function():mixed) $callback)
     {
@@ -43,18 +108,40 @@ class Deferred
         self::getQueue()->enqueue($this);
     }
 
+    public static function createAsync((function():Awaitable<mixed>) $callback):Deferred
+    {
+        $noop = () ==> null;
+        $dfd = new self($noop);
+        $dfd->async = true;
+        $dfd->asyncCallback = $callback;
+        return $dfd;
+    }
+
     public function then($onFulfilled = null, $onRejected = null)
     {
         return $this->promise->then($onFulfilled, $onRejected);
     }
 
-    private function run()
+    private function run():void
     {
-        try {
+        try
+        {
             $cb = $this->callback;
             $this->promise->resolve($cb());
-        } catch (\Exception $e) {
+        }
+        catch (\Exception $e)
+        {
             $this->promise->reject($e);
         }
+    }
+
+    private async function runAsync():Awaitable<mixed>
+    {
+        $cb = $this->asyncCallback;
+        if ($cb === null)
+        {
+            throw new \Exception('Deferred::runAsync called without async callback');
+        }
+        return await $cb();
     }
 }
